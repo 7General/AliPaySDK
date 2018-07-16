a3b086d6830e9715a965740ac7edfb4c6ee79ebe
wanghuizhou <wanghuizhou@guazi.com>
2018-07-16 11:41:30 +0800

    添加支付回调

--- Example/MSAlipaySDK/MSAppDelegate.m
+++ Example/MSAlipaySDK/MSAppDelegate.m
@@ -44,4 +44,25 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[MSAlipayManager defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"application--->>result = %@",resultDic);
+        }];
+    }
+    return YES;
+}
+
+// ios9
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+    if ([url.host isEqualToString:@"safepay"]) {
+        // 支付跳转支付宝钱包进行支付，处理支付结果
+        [[MSAlipayManager defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"application--->>result = result = %@",resultDic);
+        }];
+    }
+    return YES;
+}
+
 @end
